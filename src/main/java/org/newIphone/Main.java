d02aa98a543dba9b4c556220e50916d050ef227a
Higor Vinicius <carloshigor409@gmail.com>
2026-05-25 15:08:44 -0300

    Update Main.java

--- src/main/java/org/newIphone/Main.java
+++ src/main/java/org/newIphone/Main.java
@@ -1,4 +1,20 @@
 package org.newIphone;
 
 public class Main {
+  public static void main(String[] args){
+
+        Iphone iphone = new Iphone();
+
+        // Music section
+        iphone.playMusic();
+        iphone.selectMusic("Imagine Dragons - BELIEVER");
+
+        // Call section
+        iphone.call("81.97911-7999");
+
+        // Browser section
+        iphone.displayPage("https://google.com");
+
+
+    }
 }
